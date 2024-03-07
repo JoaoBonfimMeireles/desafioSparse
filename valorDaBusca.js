1fcac300014c69675170bf27d2b62788e723009a
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-03-06 23:46:38 -0300

    encontre o valor da busca

---
+++ valorDaBusca.js
@@ -0,0 +1,17 @@
+// usando o forEash e map// ele percorre o array e trás o valor das pesquisar do queries feitas na strings (somente o valor da pesquisa mesmo) 
+
+function matchingStrings(strings, queries) {
+    const occurrenceCount = {};
+
+    queries.forEach(query => occurrenceCount[query] = 0);
+
+    strings.forEach(string => {
+        if (occurrenceCount.hasOwnProperty(string)) {
+            occurrenceCount[string]++;
+        }
+    });
+    return queries.map(query => occurrenceCount[query]);
+
+    console.log(occurrenceCount)
+}
+

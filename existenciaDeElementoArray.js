5c69c6eea03ba12007cbf0ed806ae38e326d9e5b
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-03-11 00:04:17 -0300

    explicação

--- existenciaDeElementoArray.js
+++ existenciaDeElementoArray.js
@@ -16,7 +16,15 @@ function pangrams(s) {
 
 
 //////////////////////////////////////////// o que eu fiz não ficou tão ruim //////////////////////////////////////////////////////
-
+// toLowerCase serve para deicar letras no minusculo
+// split é para separar as palavras 
+    // O \s é para divide os caracteres por espaço "expressão regular"
+    // O * serve para indicar que isso pode acontecer mais de uma vez "expressão regular"
+// forEach cria uma função para cada elemento do array
+    // [letras] é o primeiro parametro do metodo
+// includes é para ver se exite o valor dentro do array
+// Object.values serve para tranformar o objeto como um "array" e pega os valores
+// every serve para ver todos os valores do array
 
 
 function pangrams(s) {
